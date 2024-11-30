f3720204525ca06c5e09bef08f680a404dbd6f0a
Caleb Watson <95868575+calebw12345@users.noreply.github.com>
2024-11-30 10:52:02 -0500

    Update Custom Synthesizer.py

--- pages/Custom Synthesizer.py
+++ pages/Custom Synthesizer.py
@@ -207,14 +207,6 @@ try:
     synthetic_data = pd.DataFrame(synthetic_data)
 
     x = synthetic_data.iloc[ascan_num,:]
-    synthetic_data = pd.DataFrame(synthetic_data)
-    synthetic_data = synthetic_data.to_csv()
-    st.download_button(
-        label="Download DataFrame as CSV",
-        data=csv_data,
-        file_name="example_dataframe.csv",
-        mime="text/csv"
-    )
     micros = np.arange(1300)
     micros = micros/100
     fig, ax = plt.subplots(figsize=(5, 5))
