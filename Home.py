515283b4d8a39a4de848df15d392ac926e805cb1
Caleb Watson <95868575+calebw12345@users.noreply.github.com>
2024-12-09 09:48:00 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,44 +1,11 @@
 import streamlit as st
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-from matplotlib.ticker import PercentFormatter
-import time
-# Custom HTML/CSS for the banner
-# custom_html = """
-# <div class="banner">
-#     <img src="https://brand.charlotte.edu/wp-content/uploads/sites/183/2023/04/8716-01-Charlotte-Master-File-v7_1.png" alt="Banner Image">
-# </div>
-# <style>
-#     .banner {
-#         width: 35%;
-#         height: 600px;
-#         overflow: visible;
-#     }
-#     .banner img {
-#         width: 100%;
-#         object-fit: fill;
-#     }
-# </style>
-# """
-# # Display the custom HTML
-# st.components.v1.html(custom_html)
 
-# st.set_page_config(layout="wide")
+st.set_page_config(layout="wide")
 st.image("supporting_images/logo.png")
-
-# Sidebar content
-# st.sidebar.header("Sidebar Title")
-# st.sidebar.subheader("Subheading")
-# st.sidebar.text("Produced by UNCC")
 
 # Main content
 st.title("Welcome to the Ultrasonic Testing Data Synthesizer!")
-st.write("Non-Destructive Evaluation (NDE) is a continuously changing field, but one premise has held strong against the test of time; data scarcity. No matter if you are conducting research for Ultrasonic Testing, Eddycurrent Testing, Visual Testing, Radiographic Testing, or any other type of NDE method, data scarcity is always a prevalent issue. The amount of data you have available wll heavily impact the product of your research whether it be training and qualification, developing new inspection methods, creating innovative NDE hardware, or developing cutting edge NDE software. In order to alleviate this industry-wide problem we have created this contribution to the initiative of using machine learning algorithms to synthesize NDE data. For this application, we have focused on Ultrasonic Testing (UT) data, however in the future other types of NDE data will be the subject of future investigation.")
+st.write("Non-Destructive Evaluation (NDE) is a continuously changing field, but one premise has held strong against the test of time; data scarcity. No matter if you are conducting research for Ultrasonic Testing, Eddycurrent Testing, Visual Testing, Radiographic Testing, or any other type of NDE method, data scarcity is always a prevalent issue. The amount of data you have available will heavily impact the product of your research whether it be training and qualification, developing new inspection methods, creating innovative NDE hardware, or developing cutting edge NDE software. In order to alleviate this industry-wide problem we have created this contribution to the initiative of using machine learning algorithms to synthesize NDE data. For this application, we have focused on Ultrasonic Testing (UT) data, however in the future other types of NDE data will be the subject of future investigation.")
 new_title = '<p style="font-family:sans-serif; color:rgb(0, 153, 0); font-size: 28px;">What is the Benefit of Synthesizing UT Data?</p>'
 st.markdown(new_title, unsafe_allow_html=True)
 st.write("UT data is not easy to come by. There are primarily only 2 ways to acquire UT data; to collect the data on a physical component with an encoding setup, or to acquire data via simulation softwares. Collecting data on a physical sample can be time, cost, and labor intensive. Using simulation sotwares to acquire UT data is time and cost intensive, but the data that is produced by simulation softwares often certain lack realistic features such as material and weld interface noise, coupling induced signal effects, ect.")
