160e87a1837447cdfe24840097702d906c5a7460
Caleb Watson <95868575+calebw12345@users.noreply.github.com>
2025-03-11 13:17:36 -0400

    Update Home.py
    Fix syntax error

--- Home.py
+++ Home.py
@@ -18,12 +18,12 @@ st.write("Interact with the tabs on the left side of the screen to explore the f
 col1, col2, col3 = st.columns(3)
 
 with col1:
-st.header("A cat")
-st.image("https://static.streamlit.io/examples/cat.jpg")
+  st.header("A cat")
+  st.image("https://static.streamlit.io/examples/cat.jpg")
 
 with col2:
-st.header("A dog")
-st.image("https://static.streamlit.io/examples/dog.jpg")
+  st.header("A dog")
+  st.image("https://static.streamlit.io/examples/dog.jpg")
 
 
 
